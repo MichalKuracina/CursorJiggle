3c119f2c1bc56d492787c698c90d1c3e16820af9
Michal Kuracina <michal.kuracina@henkel.com>
2022-08-28 03:24:32 +0200

    default wait 60s, cycles 60s, 2 jiggles

--- CursorJiggle/Program.cs
+++ CursorJiggle/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// https://www.pinvoke.net/default.aspx/user32.setcursorpos
+
+using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -7,27 +9,35 @@ namespace CursorJiggle
 
     internal class Program
     {
+        private static Random rnd;
+        private static int jiggleTracker;
+        private static int newX;
+        private static int newY;
+        private static int inactivityTime;
+        private static (int, int) previousCoordinates;
+        private static (int, int) currentCoordinates;
+        private static (int, int) initialCoordinates;
+
+        private static int Counter { get; set; } = 0;
+
         [DllImport("user32.dll")]
         static extern bool SetCursorPos(int X, int Y);
 
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
 
-        // https://www.pinvoke.net/default.aspx/user32.setcursorpos
-
-
-
-        public static extern bool GetCursorPos(out POINT point);
-        public static int Counter { get; set; } = 0;
-        public static bool Jiggle { get; set; } = false;
+        private static extern bool GetCursorPos(out POINT point);
 
         static void Main(string[] args)
         {
-            var previousCoordinates = (0, 0);
-            var currentCoordinates = (0, 0);
-            POINT point;
+            inactivityTime = (args.Length == 0) ? 60 : Convert.ToInt16(args[0]);
 
-            //SetCursorPos(0,0);
+            previousCoordinates = (0, 0);
+            currentCoordinates = (0, 0);
+            initialCoordinates = (0, 0);
+            POINT point;
+            rnd = new Random();
+            jiggleTracker = 0;
 
             while (true)
             {
@@ -39,25 +49,61 @@ namespace CursorJiggle
                 currentCoordinates.Item2 = y;
 
 
-                Console.WriteLine($"X: {x} Y: {y}");
+                //Console.WriteLine($"X: {x} Y: {y}");
 
-                if (true)
+                if (previousCoordinates == currentCoordinates)
                 {
-
+                    Counter++;
+                }
+                else
+                {
+                    previousCoordinates = currentCoordinates;
+                    Counter = 0;
                 }
 
+                if (Counter == 100 * inactivityTime)
+                {
+                    initialCoordinates = currentCoordinates;
+                }
 
+                if (Counter > 100 * inactivityTime)
+                {
+                    switch (jiggleTracker)
+                    {
+                        case int n when (0 <= n && n <= 25):
+                            Jiggle();
+                            break;
 
+                        case int n when (50 < n && n <= 75):
+                            Jiggle();
+                            break;
+                    }
 
+                    jiggleTracker++;
 
-
-                Counter++;
+                    if (jiggleTracker == 600)
+                    {
+                        jiggleTracker = 0;
+                    }
+                }
+                
                 Thread.Sleep(10);
             }
 
 
         }
+
+        private static void Jiggle()
+        {
+            newX = rnd.Next(-10,10) + initialCoordinates.Item1;
+            newY = rnd.Next(-10,10) + initialCoordinates.Item2;
+            SetCursorPos(newX, newY);
+            previousCoordinates.Item1 = newX;
+            previousCoordinates.Item2 = newY;
+        }
     }
+
+
 
     [StructLayout(LayoutKind.Sequential)]
     public struct POINT
